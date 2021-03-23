b16798b4e1d69380f2d60ab464a1d84cb6471a6a
ggenilson <genilsonaraujo20002002@gmail.com>
2021-03-23 18:53:42 +0100

    updates

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,9 @@ import cors from 'cors';
 
 const app = express();
 
+//middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
 app.listen(3000, () => console.log("Server is running!"));
